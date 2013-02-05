348cbacaef6201ebe7553a2c051f9f4e11eb8f06
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-02-05 19:45:27 +1000

    Add ide file to .gitignore

--- setup.py
+++ setup.py
@@ -155,9 +155,9 @@ setup(
     ext_modules = EXTENSIONS,
     classifiers=[
         'Development Status :: 3 - Alpha', 
-        'License :: OSI Approved :: MIT License', 
+        'License :: OSI Approved :: MIT License',  
         'Operating System :: OS Independent', 
-        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.2', 
         'Programming Language :: Python :: 2.7', 
         'Programming Language :: Python :: Implementation :: PyPy', 
         'Topic :: Software Development :: Compilers', 
